44f0eecce25d234f8445117e1400620369aae18a
sergiamer2004 <40940638+sergiamer2004@users.noreply.github.com>
2025-03-11 16:26:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("verify-btn").addEventListener("click", function() {
+    alert("Verification process started!");
+    // Puedes redirigir a otro sitio si es necesario:
+    // window.location.href = "https://example.com/verify";
+});
