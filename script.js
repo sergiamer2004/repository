2b37c9f6c23a42537302d5e77580ddf199817fcf
sergiamer2004 <40940638+sergiamer2004@users.noreply.github.com>
2025-03-12 11:25:07 +0100

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('verify-btn').addEventListener('click', function() {
     // Redirige a Telegram.html
-    window.location.href = '/WEB/index.html';
+    window.location.href = 'indexs.html';
 });
