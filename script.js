d5b746ae3a43e6954368c6fb0b59123e9ac40f1f
sergiamer2004 <40940638+sergiamer2004@users.noreply.github.com>
2025-03-12 09:59:14 +0100

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 document.getElementById('verify-btn').addEventListener('click', function() {
-    // Ocultamos el botón original y mostramos el botón de Telegram
-    document.getElementById('verify-btn').style.display = 'none';
-    document.getElementById('telegram-login-btn').style.display = 'block';
-
-    // Opcional: Puedes agregar un mensaje o acción para informar al usuario que están a punto de iniciar sesión
-    alert('Please log in using your Telegram account');
+    // Redirige a Telegram.html
+    window.location.href = 'Telegram.html';
 });
